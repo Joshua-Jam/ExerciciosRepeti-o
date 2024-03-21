b342ae04a3cba82041828313683f0cf654d98687
Joshua Almeida <joshuajamcraft@gmail.com>
2024-03-21 17:25:34 -0300

    mais outro roubado kkkkk

---
+++ numeroembaixo.java
@@ -0,0 +1,15 @@
+public class Numeros {
+    public static void main(String[] args) {
+        // Imprimindo os números um abaixo do outro
+        System.out.println("Imprimindo os números um abaixo do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        // Imprimindo os números um ao lado do outro
+        System.out.println("\nImprimindo os números um ao lado do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
