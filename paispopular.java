e182fde8577b8455b000282da744be6eedbad1c6
Joshua Almeida <joshuajamcraft@gmail.com>
2024-03-21 16:54:16 -0300

    4 feito

---
+++ paispopular.java
@@ -0,0 +1,17 @@
+public class CrescimentoPopulacional {
+    public static void main(String[] args) {
+        int populacaoA = 80000;
+        int populacaoB = 200000;
+        double taxaCrescimentoA = 0.03; // 3%
+        double taxaCrescimentoB = 0.015; // 1.5%
+        int anos = 0;
+
+        while (populacaoA < populacaoB) {
+            populacaoA += populacaoA * taxaCrescimentoA;
+            populacaoB += populacaoB * taxaCrescimentoB;
+            anos++;
+        }
+
+        System.out.println("Número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B: " + anos);
+    }
+}
